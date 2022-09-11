25c39378dcf7e67f1c7da3d71fa98716774cae55
Mirlan1882 <albaesenzhanov1002@gmail.com>
2022-09-11 19:39:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/activitywork/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.activitywork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.edt_txt);
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("key", editText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activitywork/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.activitywork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.txt_view);
+        String getText = getIntent().getStringExtra("key");
+        textView.setText(textView.getText().toString() + getText);
+
+    }
+}
